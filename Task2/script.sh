05d3d5fa2400c94ea3f03affca67b4dd23646191
georgy <georgy.puzakov@gmail.com>
2022-06-18 17:44:28 +0300

    add some for extra

---
+++ Task2/script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt update
+sudo apt install nginx
+touch var/www/html/index.html
+echo "Hello from George" > /var/www/html/index.html
+echo "events {
+    worker_connections 1024;
+}
+
+http {
+
+    server {
+        listen 80;
+
+        location / {
+            root /www/html;
+        }
+    }
+}" > /etc/nginx/nginx.conf
+systemctl stop nginx
+systemctl start nginx
