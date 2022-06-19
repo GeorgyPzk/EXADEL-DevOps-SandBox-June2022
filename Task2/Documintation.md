0493bae929ba03e064eaa04c9949b637ed0ecdf8
georgy <georgy.puzakov@gmail.com>
2022-06-19 03:17:15 +0300

    add extra

--- Task2/Documintation.md
+++ Task2/Documintation.md
@@ -1,2 +1,8 @@
 # Link to result
 http://52.178.108.165/
+
+
+# Extra
+
+## Peering different Virtual Networks
+You need to select virtual networks and find the peering tab. Then `Add peering`
--- Task2/script.sh
+++ Task2/script.sh
@@ -3,7 +3,8 @@ sudo apt update
 sudo apt install nginx
 touch var/www/html/index.html
 echo "Hello from George" > /var/www/html/index.html
-echo "events {
+echo "<<EOF
+events {
     worker_connections 1024;
 }
 
@@ -16,6 +17,26 @@ http {
             root /www/html;
         }
     }
-}" > /etc/nginx/nginx.conf
+}
+EOF" > /etc/nginx/nginx.conf
 systemctl stop nginx
 systemctl start nginx
+
+
+
+#echo "
+#events {
+#    worker_connections 1024;
+#}
+#
+#http {
+#
+#    server {
+#        listen 80;
+#
+#        location / {
+#            root /www/html;
+#        }
+#    }
+#}
+#" > /etc/nginx/nginx.conf
