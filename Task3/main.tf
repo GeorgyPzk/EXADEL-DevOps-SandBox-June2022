8e2dbb78588564f16e26e98f80c19c287cc68ea2
GeorgyPzk <georgy.puzakov@gmail.com>
2022-06-23 12:08:18 +0300

    fix

--- Task3/main.tf
+++ Task3/main.tf
@@ -12,11 +12,11 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
-  subscription_id   = "$azure_subscription_id"
-  tenant_id         = "$azure_subscription_tenant_id"
-  client_id         = "$service_principal_appid"
-  client_secret     = "$service_principal_password"
+
+  subscription_id   = "${{ secrets.azure_subscription_id}}"
+  tenant_id         = "${{ secrets.azure_subscription_tenant_id}}"
+  client_id         = "${{ secrets.service_principal_appid}}"
+  client_secret     = "${{ secrets.service_principal_password}}"
   
 }
 
