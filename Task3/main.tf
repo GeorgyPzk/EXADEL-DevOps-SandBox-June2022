b503750aaf9285a47e7d6311bf0f74ca1aadab17
GeorgyPzk <georgy.puzakov@gmail.com>
2022-06-25 14:02:57 +0300

    add vm2

--- Task3/main.tf
+++ Task3/main.tf
@@ -43,7 +43,7 @@ resource "azurerm_subnet" "subnetUB" {
   name                 = "${var.prefix}mySubnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.network.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 # Create Network Security Group and rule
