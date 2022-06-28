f79cc1e2e237ca5b6856bafa138984a62a235a81
GeorgyPzk <georgy.puzakov@gmail.com>
2022-06-29 00:16:08 +0300

    add 4 point

--- Task4/webservice-install.sh
+++ Task4/webservice-install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#install nginx
-apt-get -y update 
-apt-get -y install nginx
+#install apache2
+apt-get -y update
+apt-get -y install apache2
 
 # create helloworld site
-mkdir /var/www/helloworld
-cd /var/www/helloworld
-echo "Hello from ${DEVOPS}!" > index.html
+echo "Hello from ${DEVOPS}!" > /var/www/html/index.html
 
